dea4ec0c906be5f1aeea64c40025f73a51579ef4
Eddie <Eddie_Cheng@pxmart.comtw>
2023-10-17 15:26:58 +0800

    Add the root view controller and the basic layout

---
+++ TwinCalculator/TwinCalculator/View/ContainerViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ContainerViewController.swift
+//  TwinCalculator
+//
+//  Created by eddiecheng on 2023/10/17.
+//
+
+import UIKit
+import SnapKit
+
+class ContainerViewController: UIViewController {
+    
+    // MARK: Component
+    private lazy var stackView: UIStackView = {
+        let sv = UIStackView(arrangedSubviews: [leftCalculator.view, centerView, rightCalculator.view])
+        sv.distribution = .fill
+        return sv
+    }()
+    private let leftCalculator: CalculatorViewController = {
+        let lc = CalculatorViewController()
+        lc.view.backgroundColor = .lightGray
+        return lc
+    }()
+    private let centerView: UIView = {
+        let cv = UIView()
+        cv.backgroundColor = .gray
+        cv.isHidden = true
+        return cv
+    }()
+    private let rightCalculator: CalculatorViewController = {
+        let rc = CalculatorViewController()
+        rc.view.backgroundColor = .darkGray
+        return rc
+    }()
+
+    // MARK: Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initLayout()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if let traitCollection = view.window?.windowScene?.screen.traitCollection {
+            handleTrait(traitCollection)
+        }
+    }
+    
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.willTransition(to: newCollection, with: coordinator)
+        coordinator.animate(alongsideTransition: { context in
+            self.handleTrait(newCollection)
+        })
+    }
+    
+    // MARK: Layout
+    private func initLayout() {
+        view.addSubview(stackView)
+        stackView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+        leftCalculator.view.snp.makeConstraints { make in
+            make.width.equalTo(rightCalculator.view)
+        }
+        centerView.snp.makeConstraints { make in
+            make.width.equalTo(40)
+        }
+    }
+    
+    // MARK: Method
+    private func handleTrait(_ traitCollection: UITraitCollection) {
+        if traitCollection.verticalSizeClass == .regular {
+            centerView.isHidden = true
+            rightCalculator.view.isHidden = true
+        } else {
+            centerView.isHidden = false
+            rightCalculator.view.isHidden = false
+        }
+    }
+}
