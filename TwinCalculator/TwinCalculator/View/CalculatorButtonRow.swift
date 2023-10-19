6d3c945aa074344864c2790daea62189ea231ef3
Eddie <Eddie_Cheng@pxmart.comtw>
2023-10-19 14:24:07 +0800

    Add a view CalculatorButtonRow

---
+++ TwinCalculator/TwinCalculator/View/CalculatorButtonRow.swift
@@ -0,0 +1,57 @@
+//
+//  CalculatorButtonRow.swift
+//  TwinCalculator
+//
+//  Created by eddiecheng on 2023/10/17.
+//
+
+import UIKit
+
+class CalculatorButtonRow: UIView {
+    
+    let row: [CalculatorButtonItem]
+    let spacing: CGFloat = 8
+    private lazy var stackView: UIStackView = {
+        let sv = UIStackView()
+        sv.axis = .horizontal
+        sv.distribution = .fill
+        sv.spacing = self.spacing
+        return sv
+    }()
+    
+    init(row: [CalculatorButtonItem]) {
+        self.row = row
+        super.init(frame: .zero)
+        initLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func initLayout() {
+        addSubview(stackView)
+        stackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        row.forEach { item in
+            let button = CalculatorButton(title: item.title, color: .orange)
+            stackView.addArrangedSubview(button)
+        }
+        
+        for index in 1..<row.count {
+            guard let button = stackView.arrangedSubviews[index] as? CalculatorButton else { return }
+            let item = row[index]
+            if let firstBtn = stackView.arrangedSubviews.first, let firstItem = row.first {
+                let ratio = item.widthFactor/firstItem.widthFactor
+                button.snp.remakeConstraints { make in
+                    make.width
+                        .equalTo(firstBtn)
+                        .multipliedBy(ratio)
+                        .offset((ratio - 1) * spacing)
+                }
+            }
+        }
+    }
+}
